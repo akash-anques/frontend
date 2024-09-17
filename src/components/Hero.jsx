4fedbf74ed76e0a6bf2dd862a41c0cc13b6932c8
akash-anques <akash.anques@gmail.com>
2024-09-17 17:37:27 +0530

    second push

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
 
             {/* hero right section */}
                 <img className='w-full sm:w-1/2' src={assets.hero_img}  alt="" />
-
+grvbkdjrbvdsf bsdfbntgnbstgfbgfbnfsggfngfng
         </div>   
     )
 }
